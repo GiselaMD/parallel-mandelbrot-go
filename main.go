442dc3d458c8b26f473899ab33b806d4c6f081ad
Gisela Difini <>
2021-06-13 23:30:51 -0300

    deu por hoje

--- main.go
+++ main.go
@@ -5,10 +5,19 @@ import (
 	"image/color"
 	"log"
 	"math"
+	"sync"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 )
+
+type PixelColor struct {
+	x  int
+	y  int
+	cr uint8
+	cg uint8
+	cb uint8
+}
 
 type Pix struct {
 	x  int
@@ -23,17 +32,21 @@ const (
 	posY   = -1.2
 	height = 2.5
 
-	imgWidth  = 800
-	imgHeight = 800
-	maxIter   = 3000
-	samples   = 50
+	imgWidth  = 1024
+	imgHeight = 1024
+	maxIter   = 1000
+	samples   = 100
 
-	numBlocks = 16
+	numBlocks = 1936
 
 	ratio = float64(imgWidth) / float64(imgHeight)
 )
 
-var img *image.RGBA
+var (
+	img      *image.RGBA
+	mutex    sync.Mutex
+	pixColor PixelColor
+)
 
 func run() {
 	log.Println("Initial processing...")
@@ -51,6 +64,7 @@ func run() {
 	log.Println("Rendering...")
 
 	drawBuffer := make(chan Pix)
+	// done := make(chan bool, 1)
 	render(drawBuffer)
 	go draw(drawBuffer, win)
 
@@ -69,45 +83,72 @@ func main() {
 	pixelgl.Run(run)
 }
 
-func draw(drawBuffer <-chan Pix, win *pixelgl.Window) {
+func draw(drawBuffer chan Pix, win *pixelgl.Window) {
 	for i := range drawBuffer {
 		img.SetRGBA(i.x, i.y, color.RGBA{R: i.cr, G: i.cg, B: i.cb, A: 255})
-	}
 
+		// mutex.Lock()
+		// // Lock para apenas uma goroutine por vez acessar o data
+		// img.SetRGBA(pixColor.x, pixColor.y, color.RGBA{R: pixColor.cr, G: pixColor.cg, B: pixColor.cb, A: 255})
+		// mutex.Unlock()
+	}
 }
 
 func render(drawBuffer chan Pix) {
 	//TODO: dividir em seçoes a partir da quantidade de threads passada por parametro
-	for i := 0; i < numBlocks; i++ {
-		// para cada coluna: 1 go routine (thread - eu acho que o go se gerencia para nao rodar mais threads do que o SO permite)
-		// para cada ponto x,y processa a cor pelo mandelbrot
-		go func(i int, drawBuffer chan<- Pix) {
-			var sqrt = int(math.Sqrt(numBlocks))
-			// matrix width
-			for x := 0; x < i*(imgWidth/sqrt); x++ {
-				//matrix height
-				for y := 0; y < i*(imgHeight/sqrt); y++ {
-					var colorR, colorG, colorB int
-					for i := 0; i < samples; i++ {
-						a := height*ratio*((float64(x)+RandFloat64())/float64(imgWidth)) + posX
-						b := height*((float64(y)+RandFloat64())/float64(imgHeight)) + posY
-						c := pixelColor(mandelbrotIteraction(a, b, maxIter))
-						colorR += int(c.R)
-						colorG += int(c.G)
-						colorB += int(c.B)
-					}
-					var cr, cg, cb uint8
-					cr = uint8(float64(colorR) / float64(samples))
-					cg = uint8(float64(colorG) / float64(samples))
-					cb = uint8(float64(colorB) / float64(samples))
+	var sqrt = int(math.Sqrt(numBlocks))
+	for i := sqrt; i >= 0; i-- {
+		for j := 0; j <= sqrt; j++ {
 
-					drawBuffer <- Pix{
-						x, y, cr, cg, cb,
+			// para cada coluna: 1 go routine (thread - eu acho que o go se gerencia para nao rodar mais threads do que o SO permite)
+			// para cada ponto x,y processa a cor pelo mandelbrot
+
+			// TODO: x inicial e x final, y inicial e y final pra mandar pras go routines
+
+			go func(i, j int, drawBuffer chan Pix) {
+
+				// matrix width
+				// 1024 / 4
+
+				// 0 - 256
+				// 256 - 512
+				// 512 - 768
+				// 768 - 1023
+
+				// --|--|--
+				// --|--|--
+				// --|--|--
+
+				for x := i * (imgWidth / sqrt); x < (i+1)*(imgWidth/sqrt); x++ {
+					for y := j * (imgHeight / sqrt); y < (j+1)*(imgHeight/sqrt); y++ {
+						var colorR, colorG, colorB int
+						for k := 0; k < samples; k++ {
+							a := height*ratio*((float64(x)+RandFloat64())/float64(imgWidth)) + posX
+							b := height*((float64(y)+RandFloat64())/float64(imgHeight)) + posY
+							c := pixelColor(mandelbrotIteraction(a, b, maxIter))
+							colorR += int(c.R)
+							colorG += int(c.G)
+							colorB += int(c.B)
+						}
+						var cr, cg, cb uint8
+						cr = uint8(float64(colorR) / float64(samples))
+						cg = uint8(float64(colorG) / float64(samples))
+						cb = uint8(float64(colorB) / float64(samples))
+
+						// mutex.Lock()
+						// Lock para apenas uma goroutine por vez acessar o data
+						// TODO: Criar matrix de resultado por pixel
+						pixColor = PixelColor{x, y, cr, cg, cb}
+						// mutex.Unlock()
+						// done <- true
+
+						drawBuffer <- Pix{
+							x, y, cr, cg, cb,
+						}
 					}
 				}
-			}
-		}(i, drawBuffer)
-
+			}(i, j, drawBuffer)
+		}
 	}
 }
 
