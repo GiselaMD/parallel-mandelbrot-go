a8189d30bed8b6b13f803b78439d4d31e0bdc064
Gisela Difini <>
2021-06-15 20:56:19 -0300

    work and thread buffers

--- main.go
+++ main.go
@@ -10,20 +10,19 @@ import (
 	"github.com/faiface/pixel/pixelgl"
 )
 
-// type PixelColor struct {
-// 	x  int
-// 	y  int
-// 	cr uint8
-// 	cg uint8
-// 	cb uint8
-// }
-
 type Pix struct {
 	x  int
 	y  int
 	cr uint8
 	cg uint8
 	cb uint8
+}
+
+type WorkItem struct {
+	initialX int
+	finalX   int
+	initialY int
+	finalY   int
 }
 
 const (
@@ -36,13 +35,15 @@ const (
 	maxIter   = 1000
 	samples   = 100
 
-	numBlocks = 1936
+	numBlocks  = 16
+	numThreads = 10
 
 	ratio = float64(imgWidth) / float64(imgHeight)
 )
 
 var (
-	img *image.RGBA
+	img            *image.RGBA
+	workingThreads int
 	// mutex sync.Mutex
 	// pixColor PixelColor
 )
@@ -50,6 +51,7 @@ var (
 func run() {
 	log.Println("Initial processing...")
 	img = image.NewRGBA(image.Rect(0, 0, imgWidth, imgHeight))
+	workingThreads = 0
 	cfg := pixelgl.WindowConfig{
 		Title:  "Parallel Mandelbrot - PAD",
 		Bounds: pixel.R(0, 0, imgWidth, imgHeight),
@@ -62,9 +64,12 @@ func run() {
 	}
 	log.Println("Rendering...")
 
-	drawBuffer := make(chan Pix)
-	// done := make(chan bool, 1)
-	render(drawBuffer)
+	workBuffer := make(chan WorkItem, numBlocks)
+	threadBuffer := make(chan int, numThreads)
+	drawBuffer := make(chan Pix, imgWidth*imgHeight)
+
+	render(drawBuffer, workBuffer, threadBuffer)
+
 	go draw(drawBuffer, win)
 
 	for !win.Closed() {
@@ -85,70 +90,71 @@ func main() {
 func draw(drawBuffer chan Pix, win *pixelgl.Window) {
 	for i := range drawBuffer {
 		img.SetRGBA(i.x, i.y, color.RGBA{R: i.cr, G: i.cg, B: i.cb, A: 255})
-
-		// mutex.Lock()
-		// // Lock para apenas uma goroutine por vez acessar o data
-		// img.SetRGBA(pixColor.x, pixColor.y, color.RGBA{R: pixColor.cr, G: pixColor.cg, B: pixColor.cb, A: 255})
-		// mutex.Unlock()
 	}
 }
 
-func render(drawBuffer chan Pix) {
-	//TODO: dividir em seçoes a partir da quantidade de threads passada por parametro
-	var sqrt = int(math.Sqrt(numBlocks))
-	for i := sqrt; i >= 0; i-- {
-		for j := 0; j <= sqrt; j++ {
+func workerThread(workItem WorkItem, drawBuffer chan Pix, threadBuffer chan int, threadId int) {
+	for x := workItem.initialX; x < workItem.finalX; x++ {
+		for y := workItem.initialY; y < workItem.finalY; y++ {
+			var colorR, colorG, colorB int
+			for k := 0; k < samples; k++ {
+				a := height*ratio*((float64(x)+RandFloat64())/float64(imgWidth)) + posX
+				b := height*((float64(y)+RandFloat64())/float64(imgHeight)) + posY
+				c := pixelColor(mandelbrotIteraction(a, b, maxIter))
+				colorR += int(c.R)
+				colorG += int(c.G)
+				colorB += int(c.B)
+			}
+			var cr, cg, cb uint8
+			cr = uint8(float64(colorR) / float64(samples))
+			cg = uint8(float64(colorG) / float64(samples))
+			cb = uint8(float64(colorB) / float64(samples))
 
-			// para cada coluna: 1 go routine (thread - eu acho que o go se gerencia para nao rodar mais threads do que o SO permite)
-			// para cada ponto x,y processa a cor pelo mandelbrot
+			drawBuffer <- Pix{
+				x, y, cr, cg, cb,
+			}
 
-			// TODO: x inicial e x final, y inicial e y final pra mandar pras go routines
-
-			go func(i, j int, drawBuffer chan Pix) {
-
-				// matrix
-				// 1024 / 4
-
-				// 0 - 256
-				// 256 - 512
-				// 512 - 768
-				// 768 - 1023
-
-				// --|--|--
-				// --|--|--
-				// --|--|--
-
-				for x := i * (imgWidth / sqrt); x < (i+1)*(imgWidth/sqrt); x++ {
-					for y := j * (imgHeight / sqrt); y < (j+1)*(imgHeight/sqrt); y++ {
-						var colorR, colorG, colorB int
-						for k := 0; k < samples; k++ {
-							a := height*ratio*((float64(x)+RandFloat64())/float64(imgWidth)) + posX
-							b := height*((float64(y)+RandFloat64())/float64(imgHeight)) + posY
-							c := pixelColor(mandelbrotIteraction(a, b, maxIter))
-							colorR += int(c.R)
-							colorG += int(c.G)
-							colorB += int(c.B)
-						}
-						var cr, cg, cb uint8
-						cr = uint8(float64(colorR) / float64(samples))
-						cg = uint8(float64(colorG) / float64(samples))
-						cb = uint8(float64(colorB) / float64(samples))
-
-						// mutex.Lock()
-						// Lock para apenas uma goroutine por vez acessar o data
-						// TODO: Criar matrix de resultado por pixel
-						// pixColor = PixelColor{x, y, cr, cg, cb}
-						// mutex.Unlock()
-						// done <- true
-
-						drawBuffer <- Pix{
-							x, y, cr, cg, cb,
-						}
-					}
-				}
-			}(i, j, drawBuffer)
 		}
 	}
+	threadBuffer <- threadId
+}
+
+func render(drawBuffer chan Pix, workBuffer chan WorkItem, threadBuffer chan int) {
+	var sqrt = int(math.Sqrt(numBlocks))
+
+	for i := sqrt - 1; i >= 0; i-- {
+		for j := 0; j < sqrt; j++ {
+
+			// matrix
+			// 1024 / 4
+
+			// 0 - 256
+			// 256 - 512
+			// 512 - 768
+			// 768 - 1023
+
+			workBuffer <- WorkItem{
+				initialX: i * (imgWidth / sqrt),
+				finalX:   (i + 1) * (imgWidth / sqrt),
+				initialY: j * (imgHeight / sqrt),
+				finalY:   (j + 1) * (imgHeight / sqrt),
+			}
+
+		}
+	}
+
+	for i := 1; i <= numThreads; i++ {
+		threadBuffer <- i
+	}
+
+	go func(drawBuffer chan Pix, workBuffer chan WorkItem, threadBuffer chan int) {
+		for thread := range threadBuffer {
+			workItem := <-workBuffer
+			// fmt.Println("workItem", workItem)
+			// fmt.Println("thread", thread)
+			go workerThread(workItem, drawBuffer, threadBuffer, thread)
+		}
+	}(drawBuffer, workBuffer, threadBuffer)
 }
 
 func pixelColor(r float64, iter int) color.RGBA {
@@ -158,6 +164,7 @@ func pixelColor(r float64, iter int) color.RGBA {
 	// https://pt.wikipedia.org/wiki/Conjunto_de_Mandelbrot
 	if r > 4 {
 		return hslToRGB(float64(0.70)-float64(iter)/3500*r, 1, 0.5)
+		// return hslToRGB(float64(iter)/100*r, 1, 0.5)
 	}
 
 	return insideSet
